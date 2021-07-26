20c0e37436f41baf0a0dec891b6cd7b875be91c9
Arman Abkar <armanabkar@gmail.com>
2021-07-26 19:58:21 +0430

    update index.js

--- index.js
+++ index.js
@@ -45,5 +45,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  process.env.NODE_ENV === "development" &&
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
 );
