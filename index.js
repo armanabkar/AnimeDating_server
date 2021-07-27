ade0119716d58b22a189db4af290af09150a13d1
Arman Abkar <armanabkar@gmail.com>
2021-07-27 11:45:30 +0430

    update Data

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import path from "path";
 import { suggestions, characters } from "./data.js";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
@@ -13,6 +17,12 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const urlPrefix = "/api/v1";
+
+app.use("/images", express.static(__dirname + "/images"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
 
 app.get(`${urlPrefix}/characters`, (req, res) => {
   res.json(shuffle(characters));
